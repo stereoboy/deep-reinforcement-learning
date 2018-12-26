0027c186b0860762f5b14a153fde193d09249c69
Jaeyoung Lee <jylee0121@gmail.com>
2018-12-26 22:05:53 +0900

    implement dueling
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

---
+++ p1_navigation/duel_model.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DuelQNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64, fc3_units=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(DuelQNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2_val = nn.Linear(fc1_units, fc2_units)
+        self.fc2_adv = nn.Linear(fc1_units, fc2_units)
+        self.fc3_val = nn.Linear(fc2_units, 1)
+        self.fc3_adv = nn.Linear(fc2_units, action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x_val = F.relu(self.fc2_val(x))
+        x_adv = F.relu(self.fc2_adv(x))
+        val = self.fc3_val(x_val)
+        adv = self.fc3_adv(x_adv)
+        return val + adv - adv.mean()
