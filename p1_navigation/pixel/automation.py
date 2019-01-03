94bf200773f01195ff1813a7b9bebe66b7321947
Jaeyoung Lee <jylee0121@gmail.com>
2019-01-03 20:38:43 +0900

    script for test automation
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

---
+++ p1_navigation/pixel/automation.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import time
+import subprocess
+
+BEGIN_CNT=0
+TEST_SIZE=10
+
+def main():
+    try:
+        print(range(TEST_SIZE))
+        for cnt in range(BEGIN_CNT, BEGIN_CNT + TEST_SIZE):
+            print(cnt)
+            args = 'python train.py'
+            subp2 = subprocess.Popen([args], shell=True)
+            subp2.communicate()
+
+            time.sleep(3)
+    except Exception as exc:
+        print("[ERROR] {}".format(exc))
+        sys.exit()
+
+if __name__== '__main__':
+    main()
