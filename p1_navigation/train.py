235d3bce64df6158033d2260a63376fcbc56d769
Jaeyoung Lee <jylee0121@gmail.com>
2019-01-03 21:11:07 +0900

    script for training
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

---
+++ p1_navigation/train.py
@@ -0,0 +1,98 @@
+from unityagents import UnityEnvironment
+import numpy as np
+import random
+import torch
+from collections import deque
+import matplotlib.pyplot as plt
+import time
+
+env = UnityEnvironment(file_name="./Banana_Linux/Banana.x86_64")
+
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+
+# number of agents in the environment
+print('Number of agents:', len(env_info.agents))
+
+# number of actions
+action_size = brain.vector_action_space_size
+print('Number of actions:', action_size)
+
+# examine the state space 
+state = env_info.vector_observations[0]
+print('States look like:', state)
+state_size = len(state)
+print('States have length:', state_size)
+
+from prio_duel_double_dqn_agent import PrioDuelDDQNAgent
+
+agent = PrioDuelDDQNAgent(state_size=state_size, action_size=action_size, seed=0)
+
+def prio_duel_double_dqn(n_episodes=4000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+    
+    start = time.time()
+    
+    for i_episode in range(1, n_episodes+1):
+        #state = env.reset()
+        env_info = env.reset(train_mode=True)[brain_name]
+        state = env_info.vector_observations[0] 
+        score = 0
+        while True:
+            action = agent.act(state, eps)
+            #next_state, reward, done, _ = env.step(action)
+            env_info = env.step(action)[brain_name]        # send the action to the environment
+            next_state = env_info.vector_observations[0]   # get the next state
+            reward = env_info.rewards[0]                   # get the reward
+            done = env_info.local_done[0] 
+    
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+
+            if done:
+                break 
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        
+        current = time.time()
+        elapsed = current - start
+        elapsed_str = time.strftime("%H:%M:%S", time.gmtime(elapsed))
+        print('\rEpisode {}\tAverage Score: {:.2f}\t{}'.format(i_episode, np.mean(scores_window), elapsed_str), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}\t{}'.format(i_episode, np.mean(scores_window), elapsed_str))
+        if np.mean(scores_window)>=20.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            break
+    torch.save(agent.qnetwork_local.state_dict(), 'checkpoint_dueldoubledqn.pth')
+    return scores
+
+prio_duel_ddqn_scores = prio_duel_double_dqn()
+
+# plot the scores
+fig = plt.figure()
+ax = fig.add_subplot(111)
+#plt.plot(np.arange(len(dqn_scores)), dqn_scores)
+#plt.plot(np.arange(len(ddqn_scores)), ddqn_scores, color='green')
+#plt.plot(np.arange(len(duel_ddqn_scores)), duel_ddqn_scores, color='red')
+plt.plot(np.arange(len(prio_duel_ddqn_scores)), prio_duel_ddqn_scores, color='magenta')
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
