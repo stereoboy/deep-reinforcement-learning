5e047ba989ea52392ad1e3b552ec330e4bfa7e13
Jaeyoung Lee <jylee0121@gmail.com>
2019-01-04 00:08:36 +0900

    workaround for Unity's memory leak
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- p1_navigation/pixel/test.py
+++ p1_navigation/pixel/test.py
@@ -4,20 +4,30 @@ import random
 from collections import deque
 import matplotlib.pyplot as plt
 
-env = UnityEnvironment(file_name="../VisualBanana_Linux/Banana.x86")
+import logging
+
+logging.basicConfig(level=logging.NOTSET)
+logger = logging.getLogger("unityagents")
+#logger.setLevel(logging.NOTSET)
+logger.propagate = False
+env = UnityEnvironment(file_name="../VisualBanana_Windows_x86_64/Banana.exe")
 
 # get the default brain
 brain_name = env.brain_names[0]
 brain = env.brains[brain_name]
+
+env.close()
 
 action_size = 4
 
 def dqn(n_episodes=1000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
     scores = []                        # list containing scores from each episode
     scores_window = deque(maxlen=100)  # last 100 scores
-    
+
     for i_episode in range(1, n_episodes+1):
         #state = env.reset()
+        if i_episode % 100 == 1:
+            env = UnityEnvironment(file_name="../VisualBanana_Windows_x86_64/Banana.exe")    
         env_info = env.reset(train_mode=True)[brain_name]
         state = env_info.visual_observations[0]
         score = 0
@@ -40,6 +50,8 @@ def dqn(n_episodes=1000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.99
         if np.mean(scores_window)>=20.0:
             print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
             break
+        if i_episode % 100 == 0:    
+            env.close()
     return scores
 
 dqn_scores = dqn()
