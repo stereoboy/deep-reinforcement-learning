b4f3648173ccf36f96966c8b4fde85fdce57f215
Jaeyoung Lee <jylee0121@gmail.com>
2019-02-25 21:22:37 +0900

    feature: update train.py for noisy dqn
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- p1_navigation/train.py
+++ p1_navigation/train.py
@@ -29,8 +29,10 @@ state_size = len(state)
 print('States have length:', state_size)
 
 from prio_duel_double_dqn_agent import PrioDuelDDQNAgent
+from noisy_duel_double_dqn_agent import NoisyDuelDDQNAgent
 
-agent = PrioDuelDDQNAgent(state_size=state_size, action_size=action_size, seed=0)
+#agent = PrioDuelDDQNAgent(state_size=state_size, action_size=action_size, seed=0)
+agent = NoisyDuelDDQNAgent(state_size=state_size, action_size=action_size, seed=0)
 
 def prio_duel_double_dqn(n_episodes=4000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
     """Deep Q-Learning.
@@ -78,13 +80,60 @@ def prio_duel_double_dqn(n_episodes=4000, max_t=1000, eps_start=1.0, eps_end=0.0
         print('\rEpisode {}\tAverage Score: {:.2f}\t{}'.format(i_episode, np.mean(scores_window), elapsed_str), end="")
         if i_episode % 100 == 0:
             print('\rEpisode {}\tAverage Score: {:.2f}\t{}'.format(i_episode, np.mean(scores_window), elapsed_str))
-        if np.mean(scores_window)>=20.0:
+        if np.mean(scores_window)>=16.0:
             print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
             break
-    torch.save(agent.qnetwork_local.state_dict(), 'checkpoint_dueldoubledqn.pth')
+    torch.save(agent.qnetwork_local.state_dict(), 'checkpoint_noisydueldoubledqn.pth')
     return scores
 
-prio_duel_ddqn_scores = prio_duel_double_dqn()
+def noisy_duel_double_dqn(n_episodes=4000):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    
+    start = time.time()
+    
+    for i_episode in range(1, n_episodes+1):
+        #state = env.reset()
+        env_info = env.reset(train_mode=True)[brain_name]
+        state = env_info.vector_observations[0] 
+        score = 0
+        while True:
+            action = agent.act(state)
+            #next_state, reward, done, _ = env.step(action)
+            env_info = env.step(action)[brain_name]        # send the action to the environment
+            next_state = env_info.vector_observations[0]   # get the next state
+            reward = env_info.rewards[0]                   # get the reward
+            done = env_info.local_done[0] 
+    
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+
+            if done:
+                break 
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        
+        current = time.time()
+        elapsed = current - start
+        elapsed_str = time.strftime("%H:%M:%S", time.gmtime(elapsed))
+        print('\rEpisode {}\tAverage Score: {:.2f}\t{}'.format(i_episode, np.mean(scores_window), elapsed_str), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}\t{}'.format(i_episode, np.mean(scores_window), elapsed_str))
+        if np.mean(scores_window)>=16.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            break
+    torch.save(agent.qnetwork_local.state_dict(), 'checkpoint_noisydueldoubledqn.pth')
+    return scores
+
+#prio_duel_ddqn_scores = prio_duel_double_dqn()
+noisy_duel_ddqn_scores = noisy_duel_double_dqn()
 
 # plot the scores
 fig = plt.figure()
@@ -92,7 +141,8 @@ ax = fig.add_subplot(111)
 #plt.plot(np.arange(len(dqn_scores)), dqn_scores)
 #plt.plot(np.arange(len(ddqn_scores)), ddqn_scores, color='green')
 #plt.plot(np.arange(len(duel_ddqn_scores)), duel_ddqn_scores, color='red')
-plt.plot(np.arange(len(prio_duel_ddqn_scores)), prio_duel_ddqn_scores, color='magenta')
+#plt.plot(np.arange(len(prio_duel_ddqn_scores)), prio_duel_ddqn_scores, color='magenta')
+plt.plot(np.arange(len(noisy_duel_ddqn_scores)), noisy_duel_ddqn_scores, color='cyan')
 plt.ylabel('Score')
 plt.xlabel('Episode #')
 plt.show()
